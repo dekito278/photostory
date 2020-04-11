8158bc715872b6bf17a5ec402f57e08f7484d98d
dekito278 <aderizki68@gmail.com>
2020-04-11 15:25:17 +0700

    news

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class="horizontal-scroll-wrapper squares">
+  <div>item 1</div>
+  <div>item 2</div>
+  <div>item 3</div>
+  <div>item 4</div>
+  <div>item 5</div>
+  <div>item 6</div>
+  <div>item 7</div>
+  <div>item 8</div>
+</div>
+
+  </body>
+  
+<javascript>
+<link rel="stylesheet" href="assets/js/jquery.slim.min.js">
+<link rel="stylesheet" href="assets/js/bootstrap.bundle.min.js">
+</javascript>
+</html>
