8181144614a14e1bc0113c2e802f2133945b1cd8
dekito278 <aderizki68@gmail.com>
2020-04-14 06:05:16 +0700

    new

---
+++ galery.php
@@ -0,0 +1,86 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Galery</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style_galery.css" />
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/galery.js"></script>
+</head>
+<body>
+    <main>
+        <div id="landing">
+            <div id="landing-text">
+                <div id="landing-text-inner">
+                    <h1>Ade Rizki Wiranto</h1>
+                    <h2>Urban Photography</h2>
+                    <a href="#images" class="btn" id="view-work">
+                        View Work
+                    </a>
+                </div>
+            </div>
+            <div id="landing-images"></div>
+        </div>
+<div class="caption">
+    <h3>Photo One</h3>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p></div>
+<!-- Image Section -->
+<div id="images">
+<?php
+$id = $_GET ['id'];
+$query = 
+            <img src="assets/images/img-1.jpg" alt="">
+            <div class="caption">
+                <h3>Photo One</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+            <img src="assets/images/img-2.jpg" alt="">
+            <div class="caption">
+                <h3>Photo Two</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+            <img src="assets/images/img-3.jpg" alt="">
+            <div class="caption">
+                <h3>Photo Three</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+            <img src="assets/images/img-4.jpg" alt="">
+            <div class="caption">
+                <h3>Photo Four</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+            <img src="assets/images/img-5.jpg" alt="">
+            <div class="caption">
+                <h3>Photo Five</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+            <img src="assets/images/img-6.jpg" alt="">
+            <div class="caption">
+                <h3>Photo Six</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+            <img src="assets/images/img-7.jpg" alt="">
+            <div class="caption">
+                <h3>Photo Seven</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+            <img src="assets/images/img-8.jpg" alt="">
+            <div class="caption">
+                <h3>Photo Eight</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+            <img src="assets/images/img-9.jpg" alt="">
+            <div class="caption">
+                <h3>Photo Nine</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+            <img src="assets/images/img-10.jpg" alt="">
+            <div class="caption">
+                <h3>Photo Ten</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, veniam?</p>
+            </div>
+        </div>
+    </main>
+</body>
--- index.php
+++ index.php
@@ -67,7 +67,7 @@
   <div class="row">
 
     <div class="col-md-3 col-sm-6 mb-4">
-      <a href="#">
+      <a href="galery.php">
             <img class="img-fluid" src="http://placehold.it/500x300" alt="">
           </a>
     </div>
